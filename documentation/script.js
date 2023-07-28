c8442f99f73397e53a845228046dd78019a9c450
Tunji Mafe <59024298+morebymafe@users.noreply.github.com>
2023-07-28 17:51:41 +0100

    Add files via upload
    Updated the mobile view

--- documentation/script.js
+++ documentation/script.js
@@ -1,5 +1,6 @@
+//code for side nav menu
 function openNav() {
-    document.getElementById("nav-bar").style.width = "400px";
+    document.getElementById("nav-bar").style.width = "390px";
   };
 function closeNav() {
     document.getElementById("nav-bar").style.width = '0';
@@ -13,3 +14,15 @@ function closeNav() {
   })
 }
 
+//Code for change from dark mode to light mode
+function changeCSS(cssFile, cssLinkIndex) {
+
+  var oldlink = document.getElementsByTagName("link").item(cssLinkIndex);
+
+  var newlink = document.createElement("link");
+  newlink.setAttribute("rel", "stylesheet");
+  newlink.setAttribute("type", "text/css");
+  newlink.setAttribute("href", cssFile);
+
+  document.getElementsByTagName("head").item(cssLinkIndex).replaceChild(newlink, oldlink);
+}
